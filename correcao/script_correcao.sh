0efda0262a9762c8c0c92b4fd359373821e6e490
Matheus Sanchez <matheus2.sanchez@usp.br>
2018-05-29 11:45:38 -0300

    Arquivos usados para correção na pasta correcao. Executar o script depois olhar os arquivos de difs gerados com a resposta certa. Resolver isso o mais rápido possível

---
+++ correcao/script_correcao.sh
@@ -0,0 +1,144 @@
+exec=Trab1/main
+
+$exec 1 'entrada.csv' > funcionalidades1e2.txt 2>&1
+$exec 2 >> funcionalidades1e2.txt 2>&1
+
+rm -f saida.bin
+
+$exec 1 'entrada.csv' > funcionalidade3.txt 2>&1
+$exec 3 'codEscola' 35000012 >> funcionalidade3.txt 2>&1
+$exec 3 'codEscola' 35033613 >> funcionalidade3.txt 2>&1
+$exec 3 'codEscola' 35033315 >> funcionalidade3.txt 2>&1
+$exec 3 'codEscola' 1 >> funcionalidade3.txt 2>&1
+$exec 3 'dataInicio' '02/02/2012' >> funcionalidade3.txt 2>&1
+$exec 3 'dataFinal' '20/12/2012' >> funcionalidade3.txt 2>&1
+$exec 3 'municipio' 'IBIUNA' >> funcionalidade3.txt 2>&1
+$exec 3 'municipio' 'COTIA' >> funcionalidade3.txt 2>&1
+$exec 3 'municipio' 'CIDADE INEX' >> funcionalidade3.txt 2>&1
+$exec 3 'endereco' 'EST JOAO RODRIGUES DE MORAES' >> funcionalidade3.txt 2>&1
+
+$exec 5 0 >> funcionalidade3.txt 2>&1
+$exec 5 1363 >> funcionalidade3.txt 2>&1
+$exec 5 2 >> funcionalidade3.txt 2>&1
+
+$exec 3 'municipio' 'IBIUNA' >> funcionalidade3.txt 2>&1
+$exec 3 'codEscola' 35000012 >> funcionalidade3.txt 2>&1
+$exec 3 'codEscola' 35033613 >> funcionalidade3.txt 2>&1
+$exec 3 'codEscola' 35033315 >> funcionalidade3.txt 2>&1
+$exec 3 'municipio' 'COTIA' >> funcionalidade3.txt 2>&1
+
+rm -f saida.bin
+
+$exec 1 'entrada.csv' > funcionalidade4.txt 2>&1
+$exec 4 0 >> funcionalidade4.txt 2>&1
+$exec 4 1363 >> funcionalidade4.txt 2>&1
+$exec 4 52 >> funcionalidade4.txt 2>&1
+$exec 4 2363 >> funcionalidade4.txt 2>&1
+$exec 4 163 >> funcionalidade4.txt 2>&1
+$exec 4 133 >> funcionalidade4.txt 2>&1
+$exec 4 136 >> funcionalidade4.txt 2>&1
+$exec 4 113 >> funcionalidade4.txt 2>&1
+$exec 4 2 >> funcionalidade4.txt 2>&1
+
+$exec 5 0 >> funcionalidade4.txt 2>&1
+$exec 5 1363 >> funcionalidade4.txt 2>&1
+$exec 5 163 >> funcionalidade4.txt 2>&1
+$exec 5 136 >> funcionalidade4.txt 2>&1
+$exec 5 2 >> funcionalidade4.txt 2>&1
+
+$exec 4 0 >> funcionalidade4.txt 2>&1
+$exec 4 1363 >> funcionalidade4.txt 2>&1
+$exec 4 52 >> funcionalidade4.txt 2>&1
+$exec 4 2363 >> funcionalidade4.txt 2>&1
+$exec 4 163 >> funcionalidade4.txt 2>&1
+$exec 4 133 >> funcionalidade4.txt 2>&1
+$exec 4 136 >> funcionalidade4.txt 2>&1
+$exec 4 113 >> funcionalidade4.txt 2>&1
+$exec 4 2 >> funcionalidade4.txt 2>&1
+
+rm -f saida.bin
+
+$exec 1 'entrada.csv' > funcionalidade6e9.txt 2>&1
+$exec 5 0 >> funcionalidade6e9.txt 2>&1
+$exec 5 1363 >> funcionalidade6e9.txt 2>&1
+$exec 5 163 >> funcionalidade6e9.txt 2>&1
+$exec 5 136 >> funcionalidade6e9.txt 2>&1
+$exec 5 2 >> funcionalidade6e9.txt 2>&1
+$exec 5 1263 >> funcionalidade6e9.txt 2>&1
+$exec 5 1336 >> funcionalidade6e9.txt 2>&1
+$exec 5 232 >> funcionalidade6e9.txt 2>&1
+$exec 5 399 >> funcionalidade6e9.txt 2>&1
+$exec 5 402 >> funcionalidade6e9.txt 2>&1
+$exec 5 106 >> funcionalidade6e9.txt 2>&1
+$exec 5 22 >> funcionalidade6e9.txt 2>&1
+$exec 5 13 >> funcionalidade6e9.txt 2>&1
+$exec 5 16 >> funcionalidade6e9.txt 2>&1
+$exec 5 1396 >> funcionalidade6e9.txt 2>&1
+$exec 5 29 >> funcionalidade6e9.txt 2>&1
+
+$exec 2 >> funcionalidade6e9.txt 2>&1
+$exec 9 >> funcionalidade6e9.txt 2>&1
+
+$exec 6 111 '10/03/2016' '11/12/2010' 'teste' 'teste' 'test' >> funcionalidade6e9.txt 2>&1
+$exec 6 112 '10/03/2016' '11/12/2010' 'teste2' 'teste' 'test' >> funcionalidade6e9.txt 2>&1
+$exec 6 113 '10/03/2016' '11/12/2010' 'teste3' '' 'test' >> funcionalidade6e9.txt 2>&1
+$exec 6 114 '10/03/2016' '11/12/2010' 'teste' 'a' 'test' >> funcionalidade6e9.txt 2>&1
+$exec 6 115 '10/03/2016' '11/12/2010' 'teste' 'teste' 'test' >> funcionalidade6e9.txt 2>&1
+$exec 6 116 '10/03/2016' '11/12/2010' 'teste' 'd' 'test' >> funcionalidade6e9.txt 2>&1
+
+$exec 2 >> funcionalidade6e9.txt 2>&1
+$exec 9 >> funcionalidade6e9.txt 2>&1
+
+rm -f saida.bin
+
+$exec 1 'entrada.csv' > funcionalidade7.txt 2>&1
+$exec 5 0 >> funcionalidade7.txt 2>&1
+$exec 5 1363 >> funcionalidade7.txt 2>&1
+$exec 5 163 >> funcionalidade7.txt 2>&1
+$exec 5 136 >> funcionalidade7.txt 2>&1
+$exec 5 2 >> funcionalidade7.txt 2>&1
+$exec 5 1263 >> funcionalidade7.txt 2>&1
+$exec 5 1336 >> funcionalidade7.txt 2>&1
+$exec 5 232 >> funcionalidade7.txt 2>&1
+$exec 5 399 >> funcionalidade7.txt 2>&1
+$exec 5 402 >> funcionalidade7.txt 2>&1
+$exec 5 106 >> funcionalidade7.txt 2>&1
+$exec 5 22 >> funcionalidade7.txt 2>&1
+$exec 5 13 >> funcionalidade7.txt 2>&1
+$exec 5 16 >> funcionalidade7.txt 2>&1
+$exec 5 1396 >> funcionalidade7.txt 2>&1
+$exec 5 29 >> funcionalidade7.txt 2>&1
+
+$exec 7 1 111 '10/03/2016' '11/12/2010' 'teste' 'teste' 'test' >> funcionalidade7.txt 2>&1
+$exec 7 22 112 '10/03/2016' '11/12/2010' 'teste2' 'teste' 'test' >> funcionalidade7.txt 2>&1
+$exec 7 21 113 '10/03/2016' '11/12/2010' 'teste3' '' 'test' >> funcionalidade7.txt 2>&1
+$exec 7 20 114 '10/03/2016' '11/12/2010' 'teste' 'a' 'test' >> funcionalidade7.txt 2>&1
+$exec 7 43 115 '10/03/2016' '11/12/2010' 'teste' 'teste' 'test' >> funcionalidade7.txt 2>&1
+$exec 7 1369 116 '10/03/2016' '11/12/2010' 'teste' 'd' 'test' >> funcionalidade7.txt 2>&1
+
+$exec 2 >> funcionalidade7.txt 2>&1
+
+rm -f saida.bin
+
+$exec 1 'entrada.csv' > funcionalidade8.txt 2>&1
+$exec 5 0 >> funcionalidade8.txt 2>&1
+$exec 5 1363 >> funcionalidade8.txt 2>&1
+$exec 5 163 >> funcionalidade8.txt 2>&1
+$exec 5 136 >> funcionalidade8.txt 2>&1
+$exec 5 2 >> funcionalidade8.txt 2>&1
+$exec 5 1263 >> funcionalidade8.txt 2>&1
+$exec 5 1336 >> funcionalidade8.txt 2>&1
+$exec 5 232 >> funcionalidade8.txt 2>&1
+$exec 5 399 >> funcionalidade8.txt 2>&1
+$exec 5 402 >> funcionalidade8.txt 2>&1
+$exec 5 106 >> funcionalidade8.txt 2>&1
+$exec 5 22 >> funcionalidade8.txt 2>&1
+$exec 5 13 >> funcionalidade8.txt 2>&1
+$exec 5 16 >> funcionalidade8.txt 2>&1
+$exec 5 1396 >> funcionalidade8.txt 2>&1
+$exec 5 29 >> funcionalidade8.txt 2>&1
+
+$exec 8 >> funcionalidade8.txt 2>&1
+$exec 2 >> funcionalidade8.txt 2>&1
+
+rm -f saida.bin
---
+++ correcao/script_diff.sh
@@ -0,0 +1,6 @@
+diff -c --ignore-all-space --ignore-trailing-space --ignore-case funcionalidades1e2.txt RESPOSTAS/funcionalidades1e2.txt >> dif_func1e2.txt
+diff -c --ignore-all-space --ignore-trailing-space --ignore-case funcionalidade3.txt RESPOSTAS/funcionalidade3.txt >> dif_func3.txt
+diff -c --ignore-all-space --ignore-trailing-space --ignore-case funcionalidade4.txt RESPOSTAS/funcionalidade4.txt >> dif_func4.txt
+diff -c --ignore-all-space --ignore-trailing-space --ignore-case funcionalidade6e9.txt RESPOSTAS/funcionalidades6e9.txt >> dif_func6e9.txt
+diff -c --ignore-all-space --ignore-trailing-space --ignore-case funcionalidade7.txt RESPOSTAS/funcionalidade7.txt >> dif_func7.txt
+diff -c --ignore-all-space --ignore-trailing-space --ignore-case funcionalidade8.txt RESPOSTAS/funcionalidade8.txt >> dif_func8.txt
